abcbfb4b98288ba6417aadace09fdeeeee053967
farna <farna@LAPTOP-1LA7P28U>
2021-03-31 06:29:24 -0500

    First Commit

---
+++ src/javacodingpractice/HelloWorld.java
@@ -0,0 +1,19 @@
+package javacodingpractice;
+
+public class HelloWorld {
+
+	public static void main(String[] args) 
+	{
+		
+	System.out.println("Hello World");
+	
+	int x=12;
+	int y=12;
+	int z=x+y;
+	
+	System.out.println("Result = "+z);
+	
+		
+	}
+
+}
